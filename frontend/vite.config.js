5281209424eac69a256358ad714f8a01a176f5a0
Mehdi Ouhab <ouhab.me@gmail.com>
2024-07-09 12:23:49 +0200

    vercel: config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: './',
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-  }
 })
