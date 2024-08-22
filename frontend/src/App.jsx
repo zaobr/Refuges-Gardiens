9593a5992bab93c8e5bf1fbafe47e37dd9d76cc1
yassineroques <yassine.roques63@gmail.com>
2024-08-22 17:26:48 +0200

    fix inscription

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,14 @@
 import "./styles/App.css";
+import "./styles/Inscription.css";
+import "./styles/NavBarInscription.css";
+import NavBarInscription from "./components/NavBarInscription";
+import InscriptionUser from "./components/InscriptionUser";
 
 function App (){
   return (
     <>
+    <NavBarInscription />
+    <InscriptionUser />
     </>
   );
 };
---
+++ frontend/src/components/NavBarInscription.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NavBarInscription = () => {
+  return (
+    <div className='NavBar'>
+      <img src="../public/logo_rg_petit.png" alt="logo" />
+      <p className='refuges'>Refuges</p>
+      <p className='and'>&</p>
+      <p className='gardiens'>Gardiens</p>
+    </div>
+  );
+};
+
+export default NavBarInscription;
